3406fa568038e5837040e06ce8fea36a874ef114
Kejax <86169839+Kejax@users.noreply.github.com>
2021-07-18 21:55:58 +0200

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,4 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
+	# Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
@@ -13,6 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import os
 
 
 # -- Project information -----------------------------------------------------
@@ -48,10 +49,15 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    html_theme = 'sphinx_rtd_theme'
+else:
+    html_theme = 'default'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
